f48c79362d8cd97f3638bf8c3ca8a1538b3d54ef
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2020-03-31 10:07:18 +0100

    Better error handling and don't break on comments

--- authors.py
+++ authors.py
@@ -3,13 +3,14 @@ from __future__ import print_function
 import re
 import sys
 
-pattern = re.compile(r'^\\insertmydocument\{chapter\}\{(?P<title>.+?)\}\{(?P<authors>.+?)\}\{(?P<file>.+?)\}$')
+pattern = re.compile(r'^\\insertmydocument\{chapter\}\{(?P<title>.+?)\}\{(?P<authors>.+?)\}\{(?P<file>.+?)\}(?P<comment>\s*%.*)?$')
 
 def parse_authors(authors):
 	try:
-		rest, last = authors.rsplit(' and ', 1)
-		rest = rest.split(', ')
-		return [parse_author(author) for author in (rest + [last])]
+		# rest, last = authors.rsplit(' and ', 1)
+		# rest = rest.split(', ')
+		# return [parse_author(author) for author in (rest + [last])]
+		return [parse_author(author) for author in authors.split(', ')]
 	except ValueError:
 		return [parse_author(authors)]
 
@@ -24,21 +25,28 @@ def parse_author(author):
 
 def parse_filename(filename):
 	match = re.match(r'pdfs/(?P<type>\w+)/(?P<id>\d+)', filename)
+	if not match:
+		raise ValueError("Could not parse {}".format(filename))
 	return match.group('type') + '-' + match.group('id')
 
 
 if __name__ == '__main__':
-	for line in sys.stdin:
-		line = line.rstrip()
-		match = pattern.match(line)
-		if match:
-			for author in parse_authors(match.group('authors')):
-				print(r'\index[authors]{{{0}, {1}}}'.format(*author))
-			print(r'\pdfbookmark[chapter]{{{section}}}{{paper-{id}}}'.format(
-				section=match.group('title'),
-				id=parse_filename(match.group('file'))))
-		print(line)
-		if match:
-			print("")
+	try:
+		for line in sys.stdin:
+			line = line.rstrip()
+			match = pattern.match(line)
+			if match:
+				for author in parse_authors(match.group('authors')):
+					print(r'\index[authors]{{{0}, {1}}}'.format(*author))
+				print(r'\pdfbookmark[chapter]{{{section}}}{{paper-{id}}}'.format(
+					section=match.group('title'),
+					id=parse_filename(match.group('file'))))
+			print(line)
+			if match:
+				print("")
+		print(r"Done! Don't forget to enable imakeidx:\n  \usepackage{imakeidx}\n  \makeindex[name=authors,title=Index of Authors,options=-r]", file=sys.stderr)
+	except ValueError as e:
+		print(e, file=sys.stderr)
+		print(" in line {}".format(line), file=sys.stderr)
+		sys.exit(1)
 		
-	print(r"Done! Don't forget to enable imakeidx:\n  \usepackage{imakeidx}\n  \makeindex[name=authors,title=Index of Authors,options=-r]", file=sys.stderr)
